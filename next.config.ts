69da6a1908e3858ea3c89d245c1c6f47a3943074
suthinanll <suthinantong123@gmail.com>
2026-07-20 02:30:24 +0700

    แก้ไข   turbopack: {},

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,12 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development", // ปิดใช้งานตอน dev เพื่อจะได้ไม่กวนใจเวลาแก้โค้ด
   register: true,
-  skipWaiting: true,
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  turbopack: {},
   // ... ค่าคอนฟิกเดิมของคุณ (ถ้ามี)
 };
 
